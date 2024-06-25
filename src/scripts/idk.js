f6ea64e786f29a94f30e7536a741bb5e6629cb3e
parthDOOM <parthivmjasoliya9@gmail.com>
2024-06-25 11:43:06 +0530

    some improvements and additions

---
+++ src/scripts/idk.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    ScrollReveal().reveal('.section-title', { origin: 'left', distance: '50px', duration: 1500 });
+    ScrollReveal().reveal('.skills-wrapper', { origin: 'bottom', distance: '50px', duration: 1750 });
+    ScrollReveal().reveal('.skills-category', { origin: 'right', distance: '50px', duration: 1750 });
+    ScrollReveal().reveal('.skill', { origin: 'left', distance: '50px', duration: 1750, interval: 200 });
+  });
+
+
